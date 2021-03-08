8686a60c8b5b8a37d3ea87cee7be5cf1222c9731
aibigvictory <web.design.big.victory@gmail.com>
2021-03-08 15:03:21 +0900

    2021.03.08

--- js/modal.js
+++ js/modal.js
@@ -2,77 +2,51 @@ $(document).ready(function () {
     //모달 오픈을 클릭하면 동작
     $('.modal_open').on('click', function () {
         //미리 변수 선언
-        //alert()
+        var _modalIdx = $('#modal' + ($(this).parent().index() + 1)); //클릭버튼에 해당하는 모달 아이디
+        var _modal = $('.modal') //모든 모달 클래스 호출
+        //_first //모달창 첫번쨰 요소
+        //_last //모달창 마지막 요소 : 닫기버튼
+        var _modalClose = $('.modal_close')
+        var modal_close_focus = $(this) //모달닫으면 포커스 옮기기용
 
         //클릭에 해당하는 모달 외 다른 모달 및 컨텐츠 스크린리더에서 못읽게 함
         $('.modal').siblings().attr({'aria-hidden': true, inert: ''});
-
-        //모달 앞에 #dim 생성
-        $('.modal').before('<div id="dim"></div>');
-
-        //#dim이 서서히 나타나도록 효과 (stop: 여러분 클릭해도 누적효과를 모두 지우도록)
-        $('#dim').stop().fadeIn().next().css
-        
+        //모달 앞에 #dim 생성 후 변수에 저장
+        _modalIdx.before('<div id="dim"></div>');
+        var _dim = $('#dim');
+        //#dim이 서서히 나타나도록 효과 > 모달창 나타남 (stop: 여러분 클릭해도 누적효과를 모두 지우도록)
         _dim.stop().fadeIn().next().css('visibility', 'visible');
-        _first.focus();
+        //_first.focus();
 
+        //모달 닫기 버튼을 누르면
+        $(_modalClose).on('click', function () {
+            //#dim이 서서히 빠지고 다 빠지면 div 삭제
+            _dim.stop().fadeOut(function () {
+                $(this).remove();
+            });
+            //모달도 히든 처리 + 해당 모달 외 스크린리더에서 못읽게 했던 조치를 푼다
+            $(this).parent().css('visibility', 'hidden').siblings().removeAttr('aria-hidden inert');
+            //열었던 버튼으로 다시 포커스가 가도록
+            modal_close_focus.focus();
+        });
+
+        //모달 영역 밖을 클릭하면 모달을 빠져나오도록
+        _dim.on('click', function () {
+            _modalClose.click();
+        })
+
+        //esc를 누르면 모달을 빠져나옴
+        $(window).on('keydown', function (e) {
+            if (e.keyCode === 27) _modalClose.click();
+        })
+        
 
         return false;
     });
 });
 
-$(document).ready(function () {
-    //모달 오픈을 클릭하면 동작
+/* $(document).ready(function () {
     $('.md_open').on('click', function () {
-        //미리 변수 선언
-        var _openBtn = $(this);
-        var _mdCnt = $($(this).data('href'));
-        var _closeBtn = _mdCnt.find('.mdclose_btn');
-        var _first = _mdCnt.find('.first');
-        var _last = _mdCnt.find('.last');
-        var timerResize = 0;
-        console.log(_mdCnt, typeof _mdCnt);
-
-        var wrapHei = $('#wrap').height();
-        var scrollY = $(window).scrollTop();
-        console.log(wrapHei, scrollY);
-
-        //
-        $('html, body').css({height: wrapHei, overflow: 'hidden'});
-        $(window).scrollTop(scrollY);
-
-        _mdCnt.siblings().attr({'aria-hidden': true, inert: ''})
-
-        _mdCnt.before('<div id="dim"></div>')
-        var _dim = $('#dim')
-
-        $(window).on('resize', function () {
-            clearTimeout(timerResize);
-
-            timerResize = setTimeout(function () {
-                var winHei = $(this).height();
-                var winWid = $(this).width();
-                var modalHei = _mdCnt.outerHeight();
-                var modalWid = _mdCnt.outerWidth();
-                var x;
-                var y;
-                console.log(winHei,winWid,modalHei,modalWid);
-
-                if (winHei >= modalHei) {
-                    x = (winWid - modalWid) / 2
-                    y = (winHei - modalHei) / 2
-                    console.log(x,y);
-                    _mdCnt.css({left: x, top: y});
-                } else {
-                    x = (winWid - modalWid) / 2;
-                    console.log(x);
-                    _mdCnt.css({left: x, top: 0, overflowY: 'auto'});
-                }
-            }, 50);
-            $
-        });
-        $(window).trigger('resize');
-
         _dim.stop().fadeIn().next().css('visibility', 'visible');
         _first.focus();
 
@@ -91,25 +65,5 @@ $(document).ready(function () {
             }
         });
 
-        _closeBtn.on('click', function () {
-            $('html, body').removeAttr('style');
-
-            _dim.stop().fadeOut(function () {
-                $(this).remove();
-            });
-
-            _mdCnt.css({visibility: 'hidden'}).siblings().removeAttr('aria-hidden inert');
-
-            _openBtn.focus();
-        });
-
-        _dim.on('click', function () {
-            _closeBtn.click();
-        });
-
-        $(window).on('keydown', function (e) {
-            if (e.keyCode === 27) _closeBtn.click();
-        });
-
     });
-});
+}); */
